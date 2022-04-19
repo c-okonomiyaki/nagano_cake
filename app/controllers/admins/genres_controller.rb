8bc7cbf2a9bda9e96e579512d4cd5c1137c84e97
EC2 Default User <ec2-user@ip-172-31-33-184.ec2.internal>
2022-04-19 10:47:12 +0000

    ジャンル完成

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -14,6 +14,16 @@ class Admins::GenresController < ApplicationController
   end
 
   def edit
+    @genre=Genre.find(params[:id])
+  end
+
+  def update
+    @genre=Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to admins_genres_path
+    else
+      render request.referer
+    end
   end
 
   private
