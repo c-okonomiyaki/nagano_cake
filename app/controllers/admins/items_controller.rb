a0ad39abec9a32c95fcd3cec28bb44ee0ecd5cef
krkyk <madeinpink1111@gmail.com>
2022-04-19 14:08:26 +0000

    Add item-create-function

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,16 +1,43 @@
 class Admins::ItemsController < ApplicationController
   def index
     @items=Item.page(params[:page])
-    #@genre=Genre.find(params[:genre_id])
   end
 
   def show
+    @item=Item.find(params[:id])
   end
 
   def edit
+    @item=Item.find(params[:id])
+    @genres=Genre.all
+  end
+
+  def update
+    @item=Item.find(params[:id])
+    if @item.save
+      redirect_to admins_item_path(@item.id)
+    else
+      render "edit"
+    end
   end
 
   def new
     @item=Item.new
+    @genres=Genre.all
+  end
+
+  def create
+    @item=Item.new(item_params)
+    if @item.save
+      redirect_to admins_items_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_image,:item_name,:price,:introduction,:is_active,:genre_id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached:item_image
 
-  belongs_to :genre
-  has_many :carts ,dependet: :destroy
+  belongs_to :genre, optional: true
+  has_many :carts ,dependent: :destroy
   has_many :order_details ,dependent: :destroy
 end
