fa5a30058e85129cc79088093a5182429d427cec
EC2 Default User <ec2-user@ip-172-31-33-184.ec2.internal>
2022-04-20 08:08:58 +0000

    カート2

--- app/controllers/customers/homes_controller.rb
+++ app/controllers/customers/homes_controller.rb
@@ -1,6 +1,8 @@
 class Customers::HomesController < ApplicationController
   def top
-    
+    @items = Item.page(params[:page])
+    @item = Item.find(params[:id])
+    @random = Item.order("RANDOM()").limit(5)
   end
 
   def about
