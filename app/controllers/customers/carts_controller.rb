bf1901a07c01b96e017f44fadcff65074c65beb0
EC2 Default User <ec2-user@ip-172-31-33-184.ec2.internal>
2022-04-20 08:52:18 +0000

    カート3

--- app/controllers/customers/carts_controller.rb
+++ app/controllers/customers/carts_controller.rb
@@ -1,12 +1,24 @@
 class Customers::CartsController < ApplicationController
   def index
     customer = current_customer
-    @cart_item = customer.cart.item
+    @cart_items = customer.cart.item
   end
   
+  def create
+    @cart = Cart.new(cart_params)
+    @cart.customer_id = current_customer.id
+    # @cart.item_id = @item.id
+    if @cart.save
+      redirect_to item_path(@item)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
   private
   def cart_params
       params.require(:cart).permit(:item_id, :quantity)
   end
-  
+
 end
