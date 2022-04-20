7ce5071a4e74bc1c5f30356b7d36e01280c62594
anri-kbt <anrseys2@gmail.com>
2022-04-20 07:35:07 +0000

    {Add}customer-item index

--- app/controllers/customers/homes_controller.rb
+++ app/controllers/customers/homes_controller.rb
@@ -2,7 +2,6 @@ class Customers::HomesController < ApplicationController
   def top
     @items = Item.page(params[:page])
     @item = Item.new
-    @random = Item.order("RANDOM()").limit(5)
   end
 
   def about
--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -1,9 +1,14 @@
 class Customers::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page]).per(10)
+    @items = Item.where(is_active: true).page(params[:page]).per(8)
   end
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_image,:item_name,:price,:introduction,:is_active,:genre_id)
   end
 end
