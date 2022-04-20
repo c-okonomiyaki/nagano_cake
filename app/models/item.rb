19b541267912a03fe437446573a7206e912c0d3d
krkyk <madeinpink1111@gmail.com>
2022-04-20 06:00:00 +0000

    Update item-function

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached:item_image
 
-  belongs_to :genre, optional: true
+  belongs_to :genre
   has_many :carts ,dependent: :destroy
   has_many :order_details ,dependent: :destroy
 
