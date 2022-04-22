c1516877d869cea37190d81f22a62c59f2fa2628
anri-kbt <anrseys2@gmail.com>
2022-04-22 08:56:32 +0000

    {Add}admin-orders index show controller

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,7 +1,10 @@
 class Admins::OrdersController < ApplicationController
   def index
+    @orders = Order.all
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
 end
