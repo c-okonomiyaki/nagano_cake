7d4371532e5d843bd57e5de509c255de339757ab
anri-kbt <anrseys2@gmail.com>
2022-04-23 01:17:29 +0000

    {Add}admin-order index show

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -1,2 +1,15 @@
 class Admins::OrderDetailsController < ApplicationController
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    if @order_detail.update(order_detail_params)
+      redirect_to request.referer
+    else
+      redirect_to request.referer
+    end
+  end
+
+  private
+  def order_detail_params
+    params.require(:order_detail).permit(:production_status)
+  end
 end
--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -6,5 +6,22 @@ class Admins::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details
+    @order.postage = 800
+    @subtotal = @order_details.map{|order_detail|order_detail.tax_price*order_detail.quantity}
+    @total = @subtotal.sum
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to request.referer
+    else
+      redirect_to request.referer
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:order_status)
   end
 end
